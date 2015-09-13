b0a3288e1b60cba3c4f47c3c4158a1e46ced33bc
Jerod Ewert <jerodzewert@gmail.com>
2015-09-13 09:46:14 -0700

    First commit of the day.

---
+++ Algorithms/src/Algorithms.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Algorithms.cpp
+// Author      : Jerod Ewert
+// Version     :
+// Copyright   : No copyright, do as you please.
+// Description : Hello World in C++, Ansi-style, thanks eclipse!
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
