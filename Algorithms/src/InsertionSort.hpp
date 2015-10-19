68211bb5f4ae1fcbd9a40e05fcd07b16d7864eec
Jerod Ewert <jerodzewert@gmail.com>
2015-10-19 00:10:54 -0700

    Fixed the logic on this, still havent figured out the cpp syntax yet. ick.

---
+++ Algorithms/src/InsertionSort.hpp
@@ -0,0 +1,15 @@
+/*
+ * InsertionSort.hpp
+ *
+ *  Created on: Oct 18, 2015
+ *      Author: anony
+ */
+
+#ifndef INSERTIONSORT_HPP_
+#define INSERTIONSORT_HPP_
+int insertionSort();
+
+
+
+
+#endif /* INSERTIONSORT_HPP_ */
